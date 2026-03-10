79af00b439dd7b66531440e37205fbe11a429f7c
Bimd7963 <263447485+Bimd7963@users.noreply.github.com>
2026-03-10 14:43:55 +0100

    Delete .github/workflows/vite.config.js

--- .github/workflows/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // Si vous souhaitez héberger dans un sous-dossier, ex: /simulateur/
-  // décommentez la ligne ci-dessous et adaptez le chemin :
-  // base: '/undisplayed/',
-})
